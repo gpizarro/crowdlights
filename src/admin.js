34ad662d4465c1b278feee3b22f65fcdacab229f
Gonzalo Pizarro <gonzalopizarro@Gonzalos-MacBook-Air.local>
2023-03-03 14:18:00 -0500

    adds qrcode to admin

--- src/admin.js
+++ src/admin.js
@@ -2,15 +2,26 @@ import { log } from './utilities.js';
 import emmiters from './emmiters.js';
 import midiScripts from './midiScripts.js'
 import nanoKontrolScripts from './nanoKontrolScripts.js';
+import QRCode from 'qrcodejs2';
 
 const serverAddressElement = document.getElementById('serverAddress');
 const serverPortElement = document.getElementById('port');
 
+
+// Server Data
 async function getServerAddress() {
   const res = await fetch('/api');
   const data = await res.json();
   return data;
 }
+
+// QR Code Generator
+function generateQRCode(url = '') {
+  new QRCode(document.getElementById('qrCodeElement'), {
+    text: url 
+  });
+}
+
 
 document.addEventListener("DOMContentLoaded", async () => {
   let data;
@@ -19,8 +30,11 @@ document.addEventListener("DOMContentLoaded", async () => {
     data = await getServerAddress();
     serverAddressElement.innerHTML = data.ipAddress;
     serverPortElement.innerHTML = data.port;
+    let qrUrl = 'https://' + data.ipAddress + ':' + data.port;
+    generateQRCode(qrUrl);
   } catch (e) {
     console.log('Error:');
     console.log(e);
   }
 });
+
